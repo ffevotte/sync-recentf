0a93eafa32035ad9ccd45d858476dfe04a8376c3
François Févotte <fevotte@gmail.com>
2014-07-25 13:52:01 +0200

    Fix checkdoc errors

--- sync-recentf.el
+++ sync-recentf.el
@@ -26,14 +26,14 @@
 
 ;;; Commentary:
 
-;; This package helps synchronizing the recent files list between emacs
-;; instances. Without it, each emacs instance manages its own recent files
-;; list. The last one to close persistently saves its list into
+;; This package helps synchronizing the recent files list between Emacs
+;; instances.  Without it, each Emacs instance manages its own recent files
+;; list.  The last one to close persistently saves its list into
 ;; `recentf-save-file'; all files recently opened by other instances are
 ;; overwritten.
 ;;
-;; With sync-recentf, all running emacs instances periodically synchronize their
-;; local recent files list with `recentf-save-file'. This ensures that all
+;; With sync-recentf, all running Emacs instances periodically synchronize their
+;; local recent files list with `recentf-save-file'.  This ensures that all
 ;; instances share the same list, which is persistently saved across sessions.
 
 ;; `sync-recentf-marker' is always pushed on top of `recentf-list' when it is
@@ -41,12 +41,12 @@
 ;;
 ;; All files appearing before `sync-recentf-marker' in `recentf-list' were
 ;; visited after the last synchronization, meaning that they should be pushed up
-;; in the next synchronization phase. Synchronization actually happens at file
+;; in the next synchronization phase.  Synchronization actually happens at file
 ;; save (see `recentf-save-list') or during periodical cleanups (see
 ;; `recentf-cleanup')
 
 ;; If you make improvements to this code or have suggestions, please do not
-;; hesitate to fork the repository or submit bug reports on github. The
+;; hesitate to fork the repository or submit bug reports on github.  The
 ;; repository is at:
 ;;
 ;;     https://github.com/ffevotte/sync-recentf
@@ -72,10 +72,11 @@
   (recentf-push sync-recentf-marker))
 
 (defun sync-recentf--marker-top-p ()
-  "non-nil if `sync-recentf-marker' is on top of `recentf-list'."
+  "Non-nil if `sync-recentf-marker' is on top of `recentf-list'."
   (string= (car recentf-list) sync-recentf-marker))
 
 (defun sync-recentf--to-sync ()
+  "List of files which have been visited since the last synchronization."
   (let ((to-sync nil)
         (recent-files recentf-list))
     (while (and recent-files
@@ -89,7 +90,7 @@
 
 Every file in `recentf-list' more recent (i.e. before)
 `sync-recentf-marker' will be pushed in front of the list coming
-from `recentf-save-file'. The result of the merge is put into
+from `recentf-save-file'.  The result of the merge is put into
 `recentf-list'.
 
 Return nil if no merge was needed."
@@ -118,7 +119,7 @@ Return nil if no merge was needed."
   "Save the recent files list.
 
 The original version of this function is accessible through
-`sync-recentf-save-list'. This version has the same effect,
+`sync-recentf-save-list'.  This version has the same effect,
 except it synchronizes the recent files list before saving it to
 `recentf-save-file'."
   (when (sync-recentf--sync)
@@ -133,7 +134,7 @@ except it synchronizes the recent files list before saving it to
           (lambda (file opponent)
             (message "sync-recentf: file locked, aborting.")
             ;;(signal 'file-locked (list file opponent))
-            (error "file `%s' locked by `%s'" file opponent)))
+            (error "File `%s' locked by `%s'" file opponent)))
     (unwind-protect
         (let ((warning-suppress-types 'emacs))
           (sync-recentf-save-list))
